988d5f3344c9ffa48a3d3e38ad31dd28f31a714f
Eric <eric.danowski@cengage.com>
2018-10-22 06:32:50 -0400

    inject after footer

--- index2.js
+++ index2.js
@@ -5,80 +5,41 @@ var http = require('http'),
     fileSystem = require('fs'),
     path = require('path');
 
-
+var simpleSelectElemntCount = 0;
 var selects = [];
 var simpleselect = {};
 var headSelect = {};
 
-headSelect.query = 'body';
+headSelect.query = 'footer';
 headSelect.func = function(node) {
     var stm = node.createStream({ "outer" : true });
-
     var tag = '';
-
-    //collect all the data in the stream
     stm.on('data', function(data) {
        tag += data;
     });
-
-    //When the read side of the stream has ended..
     stm.on('end', function() {
-
-      //Print out the tag you can also parse it or regex if you want
-    //   process.stdout.write('tag:   ' + tag + '\n');
-    //   process.stdout.write('end:   ' + node.name + '\n');
-      
-      //Now on the write side of the stream write some data using .end()
-      //N.B. if end isn't called it will just hang.
-      
       stm.end(tag + '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/rythm.js/2.2.4/rythm.min.js"></script>' +
-      '<script>var rythm = new Rythm();rythm.setMusic("http://localhost:8000/test.mp3");rythm.addRythm("twist3", "twist", 0, 10, { direction: "left" });setTimeout(function() {rythm.start();}, 5000);</script>');      
+      '<script>var rythm = new Rythm();rythm.setMusic("http://localhost:8000/test.mp3");rythm.addRythm("shake3", "shake", 0, 10, { direction: "left", min: 5, max: 100 });rythm.addRythm("twist1", "twist", 0, 10);rythm.addRythm("twist3", "twist", 0, 10, { direction: "left" });setTimeout(function() {rythm.start();}, 5000);</script>');      
     });    
-
 };
+
 selects.push(headSelect);
-//<img id="logo" src="/images/logo.svg" alt="node.js">
 var rhythmClasses = ['rythm-medium', 'rythm-high', 'rythm-bass'];
 simpleselect.query = 'div[tooltip*=-]';
-
 simpleselect.func = function (node) {
-    // console.log(node)
-    
-    //Create a read/write stream wit the outer option 
-    //so we get the full tag and we can replace it
     var currentClass = node.getAttribute('class');
-    node.setAttribute('class', currentClass + ' ' + rhythmClasses[Math.floor(Math.random()*rhythmClasses.length)]);
-    // var stm = node.createStream({ "outer" : true });
-
-    // //variable to hold all the info from the data events
-    // var tag = '';
-
-    // //collect all the data in the stream
-    // stm.on('data', function(data) {
-    //    tag += data;
-    // });
-
-    // //When the read side of the stream has ended..
-    // stm.on('end', function() {
-
-    //   //Print out the tag you can also parse it or regex if you want
-    // //   process.stdout.write('tag:   ' + tag + '\n');
-    // //   process.stdout.write('end:   ' + node.name + '\n');
-      
-    //   //Now on the write side of the stream write some data using .end()
-    //   //N.B. if end isn't called it will just hang.  
-    //   stm.end('<img id="logo" src="http://i.imgur.com/LKShxfc.gif" alt="node.js">');      
-    
-    // });    
+    var twist = ' shake3';
+    if (currentClass.indexOf('successful') > -1) {
+        simpleSelectElemntCount++;
+        twist = ' twist1';
+    }
+    // node.setAttribute('class', currentClass + ' ' + rhythmClasses[Math.floor(Math.random()*rhythmClasses.length)] + ' twist3');
+    node.setAttribute('class', currentClass + ' ' + rhythmClasses[Math.floor(Math.random()*rhythmClasses.length)] + twist);
 }
 
 selects.push(simpleselect);
 
-//
-// Basic Connect App
-//
 var app = connect();
-
 var proxy = httpProxy.createProxyServer({
    target: 'http://jenkins-as01.gale.web:8080',
    agent  : http.globalAgent, 
@@ -87,23 +48,18 @@ var proxy = httpProxy.createProxyServer({
    followRedirects: true
 })
 
-
 app.use(require('harmon')([], selects, true));
 
 app.use(
   function (req, res) {
       if (req.url && req.url === '/test.mp3') {
-
         var filePath = path.join(__dirname, 'test.mp3');
         var stat = fileSystem.statSync(filePath);
-
         res.writeHead(200, {
             'Content-Type': 'audio/mpeg',
             'Content-Length': stat.size
         });
-
         var readStream = fileSystem.createReadStream(filePath);
-        // We replaced all the event handlers with a simple call to util.pump()
         readStream.pipe(res);
       } else {
         proxy.web(req, res);
