44acdad94f17bffa655b5eaccf037729b56a2d01
Eric <eric.danowski@cengage.com>
2019-04-24 11:37:37 -0400

    adding specs and change client script so its not a node module

--- clientScript.js
+++ clientScript.js
@@ -1,15 +1,49 @@
-function jobStatusCache() {
-    jQuery('div[tooltip]').each(function() {
-        localStorage.setItem(jQuery(this).attr('title'), 'foo');
-    });
-}
+const STATUS = ["successful", "failing", "claimed"];
 
+function jobStatusCache(proxyPort = 8000) {
+  let jobStatusCache = localStorage.getItem("jobStatusCache");
+  let storedCache = jobStatusCache ? new Map(JSON.parse(jobStatusCache)) : new Map();
+
+  jQuery("div.job").each(function() {
+    let $element = jQuery(this);
+    let currentStatus = $element
+      .attr("class")
+      .split(" ")
+      .filter(c => STATUS.includes(c))
+      .shift();
+
+      let lastStatus = storedCache.get($element.attr('title')) || '';
+      if (currentStatus !== lastStatus && currentStatus === 'claimed') {
+        $element.addClass('flip-card').wrapInner('<div class="flip-card-front"></div>');
+        let imgs = $element.find('img').clone();
+        let container = jQuery('<div class="flip-card-back"></div>');
+        container.append(imgs);
+        $element.append(container);
+        $element.wrapInner('<div class="flip-card-inner"></div>'); 
+      }
+
+      storedCache.set($element.attr("title"), currentStatus);
+  });
+  localStorage.setItem('jobStatusCache', JSON.stringify([...storedCache]));
+
+  if (jQuery('.flip-card').length > 0) {
+        jQuery('#main-panel').append(`<audio class="audios" id="survey-says" controls preload="none"><source src="http://localhost:${proxyPort}/survey-said.mp3" type="audio/mpeg"></audio>`);
+        jQuery('#survey-says').on('ended', function() {
+            jQuery('.flip-card').toggleClass('flip');
+            setTimeout(function() {
+                jQuery('.flip-card').toggleClass('flip');
+            }, 5000);
+        });
+        jQuery('#survey-says').trigger('play')
+    }
+
+}
 
 // const jobStatusCache = function jobStatusCache() {
 //     let storedCache = localStorage.getItem('jobStatusCache');
 //     let cache = storedCache ? new Map(JSON.parse(storedCache)) : new Map();
 //     jQuery('div.job:not(.successful)').each(function() {
-//         let currentStatus = jQuery(this).find('.culprit').length > 0 ? 'unclaimed' : 'claimed'; 
+//         let currentStatus = jQuery(this).find('.culprit').length > 0 ? 'unclaimed' : 'claimed';
 //         let lastStatus = cache.get(jQuery(this).attr('title')) || 'unclaimed';
 //         cache.set(jQuery(this).attr('title'), currentStatus);
 //         if (lastStatus === 'unclaimed' && currentStatus === 'claimed') {
@@ -20,10 +54,10 @@ function jobStatusCache() {
 //             jQuery(this).append(container);
 //             jQuery(this).wrapInner('<div class="flip-card-inner"></div>');
 //         }
-        
+
 //     });
 
-//     localStorage.setItem('jobStatusCache', JSON.stringify([...cache])); 
+//     localStorage.setItem('jobStatusCache', JSON.stringify([...cache]));
 
 //     if (jQuery('.flip-card').length > 0) {
 //         jQuery('#main-panel').append('<audio class="audios" id="survey-says" controls preload="none"><source src="http://localhost:8000/survey-said.mp3" type="audio/mpeg"></audio>');
@@ -39,12 +73,10 @@ function jobStatusCache() {
 
 // module.exports = jobStatusCache;
 
-
-
-
 //Now building: #1257, 99% - N/A left
 
 //Possible culprit: shubhamgovil, himanshu.dash, anshumanambasht
 
 //claimed
 //failing
+//successful
--- test/clientScriptSpec.js
+++ test/clientScriptSpec.js
@@ -1,12 +1,136 @@
-describe("Client spec", () => {
-  it("caches the jobs", () => {
-    let localStorageSpy = sinon.spy(window.localStorage, "setItem");
+describe("client spec", () => {
+  let localStorageGetItemStub, localStorageSetItemStub, triggerStub;
+
+  beforeEach(() => {
+    localStorageGetItemStub = sinon.stub(window.localStorage, "getItem");
+    localStorageSetItemStub = sinon.stub(window.localStorage, "setItem");
+    triggerStub = sinon.stub(jQuery.fn, 'trigger');
+  });
+
+  afterEach(() => {
+    localStorageSetItemStub.restore();
+    localStorageGetItemStub.restore();
+    triggerStub.restore();
+    $('.fixture').remove();
+  });
+
+  it("should retrieve cache", () => {
+    jobStatusCache();
+
+    sinon.assert.calledWith(localStorageGetItemStub, "jobStatusCache");
+  });
+
+  it("should not throw if cache not in localstorage", () => {
+    localStorageGetItemStub.withArgs('jobStatusCache').returns(undefined);
+    
+    jobStatusCache();
+
+    sinon.assert.calledWith(localStorageGetItemStub, "jobStatusCache");
+  });
+
+  it("should cache jobs status", () => {
     $("body").append(`
-        <div tooltip="job-1" title="job-1"></div>  
+        <div class="fixture">
+            <div class="job failing" title="job-1"></div> 
+            <div class="job successful" title="job-2"></div>  
+            <div class="job claimed" title="job-3"></div> 
+        </div> 
     `);
     jobStatusCache();
 
-    localStorageSpy.restore();
-    sinon.assert.calledWith(localStorageSpy, 'job-1', 'job-1');
+    sinon.assert.calledWith(
+      localStorageSetItemStub,
+      "jobStatusCache",
+      '[["job-1","failing"],["job-2","successful"],["job-3","claimed"]]'
+    );
   });
+
+  describe('if status changed', () => {
+
+    it("should NOT add class or wrap elements if new status is NOT claimed", () => {
+        localStorageGetItemStub.withArgs('jobStatusCache').returns('[["job-1","failing"],["job-2","successful"],["job-3","claimed"]]');
+        $("body").append(`
+            <div class="fixture">
+                <div id="job1" tooltip="job-1" class="job successful building" title="job-1"><div>inner</div></div>
+            </div>
+        `);
+        jobStatusCache();
+    
+        chai.expect($('#job1').hasClass('flip-card')).to.equal(false);
+        chai.expect($('#job1').html()).to.equal('<div>inner</div>');
+    });
+
+    it("should add class and wrap inner elements", () => {
+        localStorageGetItemStub.withArgs('jobStatusCache').returns('[["job-1","failing"],["job-2","successful"],["job-3","claimed"]]');
+        $("body").append(`
+            <div class="fixture">
+                <div id="job1" tooltip="job-1" class="job claimed building" title="job-1"><div>inner</div></div>
+            </div>
+        `);
+        jobStatusCache();
+    
+        chai.expect($('#job1').hasClass('flip-card')).to.equal(true);
+        chai.expect($('#job1').html()).to.equal('<div class="flip-card-inner"><div class="flip-card-front"><div>inner</div></div><div class="flip-card-back"></div></div>');
+      });
+    
+      it("should add element with cloned images", () => {
+        localStorageGetItemStub.withArgs('jobStatusCache').returns('[["job-1","failing"],["job-2","successful"],["job-3","claimed"]]');
+        $("body").append(`
+            <div class="fixture">
+                <div id="job1" tooltip="job-1" class="job claimed building" title="job-1"><div><img src="img/1" /><img src="img/2" /></div></div>
+            </div>
+        `);
+        jobStatusCache();
+       
+        chai.expect($('#job1 .flip-card-inner').html()).to.equal('<div class="flip-card-front"><div><img src="img/1"><img src="img/2"></div></div><div class="flip-card-back"><img src="img/1"><img src="img/2"></div>');
+      });
+
+      it("should add audio element and trigger play if there are flip cards", () => {
+        $("body").append(`
+            <div class="fixture">
+                <div class="flip-card"></div>
+                <div id="main-panel"></div>
+            </div>
+        `);
+        jobStatusCache(8000);
+       
+        sinon.assert.calledWith(
+            triggerStub,
+            "play"
+        );
+        chai.expect($('#main-panel').html()).to.equal('<audio class="audios" id="survey-says" controls="" preload="none"><source src="http://localhost:8000/survey-said.mp3" type="audio/mpeg"></audio>');
+      });
+
+      it("should toggle class when audio play is ended and after timeout", () => {
+        let clock = sinon.useFakeTimers();
+
+        $("body").append(`
+            <div class="fixture">
+                <div class="flip-card"></div>
+                <div id="main-panel"></div>
+            </div>
+        `);
+        jobStatusCache(8000);
+
+        triggerStub.restore();
+        $('#survey-says').trigger('ended');
+
+        chai.expect($('.flip-card').hasClass('flip')).to.equal(true);
+        clock.tick(2500);
+        chai.expect($('.flip-card').hasClass('flip')).to.equal(true);
+        clock.tick(2501);
+        chai.expect($('.flip-card').hasClass('flip')).to.equal(false);
+        clock.restore();
+      });
+  });
+  
 });
+
+/*
+<div id="job1" tooltip="job-1" class="job successful building" title="job-1">
+            <p><a href="#">job-1</a></p>
+            <p>Claiming for somebody (Body, Some).</p>
+            <p></p>
+            <p>Possible culprit: someone.else</p>
+        </div>
+*/
